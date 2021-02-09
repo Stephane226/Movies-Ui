d908b2eed59627cd9a44c5655aefb358c1abc021
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2021-02-09 21:40:15 +0300

    2 Components have been created

---
+++ movieui/src/components/blockApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import useState from 'react'
+
+function BlockApp(){
+  
+
+
+    return(
+        <div>
+            hello bog
+        </div>
+    )
+}
+
+
+
+export default  BlockApp
