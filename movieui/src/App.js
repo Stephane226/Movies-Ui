233d0a2e5c3c232da651924e921b0c0b92d42f5e
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2021-02-09 21:43:20 +0300

    Header and Footer components have been created

--- movieui/src/App.js
+++ movieui/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import BlockApp from './components/blockApp';
-import Ui from './components/ui'
+import Ui from './components/ui';
+import Header from './components/header' ;
+import Footer from './components/footer' 
 
 function App() {
   return (
     <div className="App">
-     <BlockApp/>
-     <Ui/>
+     
     </div>
   );
 }
